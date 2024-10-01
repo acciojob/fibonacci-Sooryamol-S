057e8bc73b4addc0ed48afd5c1a4d00297abbf34
Sooryamol-S <135025674+Sooryamol-S@users.noreply.github.com>
2024-10-01 19:02:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 function fibonacci(num) {
 // your code here
+if(num==0){
+	return 0;
+}
+if(num==1){
+	return 1;
+}
+	let a=0,b=1;
+	for(let i=2;i<=num;i++){
+		c=a+b;
+		a=b;
+		b=c;
+	}
+	return c;
 }
 
 module.exports = fibonacci;
