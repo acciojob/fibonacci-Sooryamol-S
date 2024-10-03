d21c35da1db1fc3b3c20ff3c153630896d4206b8
Sooryamol-S <135025674+Sooryamol-S@users.noreply.github.com>
2024-10-03 11:15:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 function fibonacci(num) {
 // your code here
-if(num==0){
+if(num===1){
 	return 0;
 }
-if(num==1){
+if(num===2){
 	return 1;
 }
-	let a=0,b=1;
-	for(let i=2;i<=num;i++){
+	let a=0,b=1,c=0;
+	for(let i=3;i<=num;i++){
 		c=a+b;
 		a=b;
 		b=c;
